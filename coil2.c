0c7d0c4cd5f9aa05850e959a5ae427b4313fcac1
tozturk18 <tunaozturk2001@hotmail.com>
2023-05-26 12:11:48 -0400

    new mode added

--- coil2.c
+++ coil2.c
@@ -11,6 +11,7 @@
  * and then this file will directly into it.
  * 
  * Parameters:
+ *  -   kicad_pcb:      Determines the kicad_pcb file address
  *  -   turns:          Determines the number of turns per coil
  *  -   innerRadius:    Determines the inner radius of the coil
  *  -   spacing:        Determines the spacing between each spiral in the coil
@@ -26,6 +27,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 /* --- End of IMPORTS --- */
 
 /* --- MAIN --- */
@@ -33,12 +35,16 @@ int main(int argc, char *argv[]) {
 
     /* --- FAILSAFE --- */
     if(argc > 9) {
-        printf("Usage: %s turns innerRadius spacing start_X start_Y layers direction(1 or -1) width(default 0.25)) \n",argv[0]);
+        printf("Usage: %s kicad_pcb_file turns innerRadius spacing start_X start_Y layers direction(1 or -1) width(default 0.25)) \n",argv[0]);
         return 1;
     }
     /* --- End of FAILSAFE --- */
 
     /* --- CONSTANTS --- */
+    // kicad_pcb file address on your computer.
+    // This is footprint file to overwrite the coil into
+    char* filename = "./coil.kicad_pcb";
+
     // Amount of Turns around the center
     float turns = 10;           // Default 10 rotations
 
@@ -69,47 +75,67 @@ int main(int argc, char *argv[]) {
      * ./spiral.o 1 1 That will change the turns and the inner radius. 
      */
     switch (argc){
-        case 9:
-            // Run all the Switch Statement below until the break if 9 arguments entered
+        case 10:
+            // Run all the Switch Statement below until the break if 10 arguments entered
             width = atof(argv[8]);      // Update the Width
             // Failsafe for width
             if (width < 0) { width = 0.25;  }
-        case 8:
+        case 9:
             // Run all the Switch Statement below until the break if 8 arguments entered
             direction = atoi(argv[7]);  // Update the Direction
             // Failsafe for direction
             if (direction != 1 || direction != -1) { direction = 1; }
-        case 7:
+        case 8:
             // Run all the Switch Statement below until the break if 7 arguments entered
             layers = atoi(argv[6]);     // Update the Layers
             // Failsafe for layers
             if (layers < 1) { layers = 1; }
-        case 6:
+        case 7:
             // Run all the Switch Statement below until the break if 6 arguments entered
             startX = atof(argv[4]);     // Update the Start Coordinate X value
             startY = atof(argv[5]);     // Update the Start Coordinate Y value
+        case 5:
+            // Run all the Switch Statement below until the break if 5 arguments entered
+            spacing = atof(argv[3]) + width;    // Update the Spacing
+            if (spacing == 0) { spacing = 0.0001; }
         case 4:
             // Run all the Switch Statement below until the break if 4 arguments entered
-            spacing = atof(argv[3]) + width;    // Update the Spacing
-            // Failsafe for spacing
-            if (spacing == 0) { spacing = 0.0001; }
-        case 3:
-            // Run all the Switch Statement below until the break if 3 arguments entered
             innerRadius = atof(argv[2]);// Update the Inner Radius
             // Failsafe for innerRadius
             if (innerRadius < 0) { innerRadius = 0; }
-        case 2:
-            // Run all the Switch Statement below until the break if 2 arguments entered
+        case 3:
+            // Run all the Switch Statement below until the break if 3 arguments entered
             turns = atof(argv[1]);      // Update the Turns
             // Failsafe for turns
             if (turns < 1) { turns = 1; }
             break;
-        case 5:
-            // If 5 arguments entered then, the start position is entered wrongly. Send an error message
+        case 2:
+            // Run all the Switch Statement below until the break if 2 arguments entered
+            filename = argv[1];      // Update the Turns
+            break;
+        case 6:
+            // If 6 arguments entered then, the start position is entered wrongly. Send an error message
             printf("Usage: %s turns innerRadius spacing start_X start_Y layers direction(1 or -1) width(default 0.25)) \n",argv[0]);
             return 1;
     }
     /* --- End of ARGUMENTS --- */
+
+    /* --- kicad_pcb Footprint File --- */
+    // Create a file variable
+    FILE *fp;
+
+    // Open the footprint file
+    fp = fopen(filename,"w");
+
+    // Check if the file opened
+    if (fp == NULL) {
+        printf("Error opening kicad_pcb file! kicad_pcb file address entered wrong!\n\r");   
+        return(1);             
+    }
+
+    // Set the cursor to the very beginning of the file
+    fseek(fp, 0, SEEK_SET);
+    /* --- End of kicad_pcb Footprint File --- */
 
     /* --- Start & End Positions ---  */
     float start = innerRadius;              // Start Position
@@ -162,7 +188,11 @@ int main(int argc, char *argv[]) {
     /* --- End of GENERATE COIL --- */
 
 
+    
+
     /* --- DISPLAY --- */
+    printf("Start writing into %s\n\r", filename);
+
     // Iterate through each copper layer
     for (int i = 0; i < layers; i++) {
         // Iterate through each position on the coil
@@ -170,17 +200,21 @@ int main(int argc, char *argv[]) {
             // Print out the wire segments according to KiCAD Footprint File.
 
             if (i == 0) {
-                printf("(segment (start %f %f) (end %f %f) (width %f) (layer \"F.Cu\") (net 0) (tstamp 4efbfedb-0d6a-488e-863f-1beaaa%dba%d))\n\r", xPos[i][j], yPos[i][j], xPos[i][j+1], yPos[i][j+1], width, j, i);
+                fprintf(fp, "(segment (start %f %f) (end %f %f) (width %f) (layer \"F.Cu\") (net 0) (tstamp 4efbfedb-0d6a-488e-863f-1beaaa%dba%d))\n", xPos[i][j], yPos[i][j], xPos[i][j+1], yPos[i][j+1], width, j, i);
             } else {
-                printf("(segment (start %f %f) (end %f %f) (width %f) (layer \"B.Cu\") (net 0) (tstamp 4efbfedb-0d6a-488e-863f-1beaaa%dba%d))\n\r", xPos[i][j], yPos[i][j], xPos[i][j+1], yPos[i][j+1], width, j, i);
+                fprintf(fp, "(segment (start %f %f) (end %f %f) (width %f) (layer \"B.Cu\") (net 0) (tstamp 4efbfedb-0d6a-488e-863f-1beaaa%dba%d))\n", xPos[i][j], yPos[i][j], xPos[i][j+1], yPos[i][j+1], width, j, i);
             }
         }
     }
-    /* --- End of DISPLAY --- */
 
     // Add vias
-    printf("(via (at %f %f) (size 0.8) (drill 0.4) (layers \"F.Cu\" \"B.Cu\") (free) (net 0) (tstamp e5f06cd2-492e-41b2-8ded-13a3fa1042b%d))\n\r", xPos[0][0], yPos[0][0], 0);
-    
+    fprintf(fp,"(via (at %f %f) (size 0.8) (drill 0.4) (layers \"F.Cu\" \"B.Cu\") (free) (net 0) (tstamp e5f06cd2-492e-41b2-8ded-13a3fa1042b%d))\n", xPos[0][0], yPos[0][0], 0);
+    printf("End of writing.\n\r");
+    /* --- End of DISPLAY --- */
+
+    // Close the File
+    fclose(fp);
+
     // End script
     return 0;
 }
