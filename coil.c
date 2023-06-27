80497205be00200a8d1f1a89baac4ff826d1027a
tozturk18 <tunaozturk2001@hotmail.com>
2023-06-27 15:15:50 +0300

    code clean up

--- coil.c
+++ coil.c
@@ -31,18 +31,13 @@
 #include <string.h>
 /* --- End of IMPORTS --- */
 
-/* --- User Defined Functions --- */
-
-// Check if the arguments entered by the user is correct
-// If all correct, returns the number for which parameter to populate
-// If wrong return 0 and display an error message
-int argvChecker(char *argv[]);
-/* --- End of User Defined Functions --- */
-
 /* --- MAIN --- */
 int main(int argc, char *argv[]) {
 
     /* --- FAILSAFE --- */
+    /* Make sure the program does not fail due to any unforseen user errors
+     */
+
     // Check if correct amount of arguments entered
     if(argc % 2 == 0) {
         printf("Usage: %s flags parameters\n\r",argv[0]);
@@ -52,6 +47,14 @@ int main(int argc, char *argv[]) {
     /* --- End of FAILSAFE --- */
 
     /* --- CONSTANTS --- */
+    /* Below are the initial coil parameters used to create a coil
+     * even if the user only enters one or two parameters. This ensures
+     * that the program will be able to create a coil even without any user
+     * defined parameters. When run without any user defined parameters, it
+     * will create a basic coil using the constants defined below. This will
+     * allow for debuging and easy learning curve for beginner users.
+     */
+
     // kicad_pcb file address on your computer.
     // This is footprint file to overwrite the coil into
     char* filename = "./coil_text";
@@ -156,10 +159,14 @@ int main(int argc, char *argv[]) {
     /* --- End of ARGUMENTS --- */
 
     /* --- kicad_pcb Footprint File --- */
+    /* Open or create a new file in write mode to write all the segment and via coordinates.
+     * IF THE DOCUMENT ALREADY HVE DATA IN IT, IT WILL BE OVERWRITTEN.
+     */
+
     // Create a file variable
     FILE *fp;
 
-    // Open the footprint file
+    // Open the file in write mode
     fp = fopen(filename,"w");
 
     // Check if the file opened
@@ -178,7 +185,10 @@ int main(int argc, char *argv[]) {
     /* --- End of Start & End Positions --- */
 
     /* --- ANGLE --- */
-    // Calculate the angle difference between the original spiral and the spaced one
+    /* Calculate the angle difference between the original spiral and the spaced one.
+     * Adjust the angle direction according to the final y position on the spaced coil.
+     * Include the user rotation into the coil angle.
+     */
 
     // Find the start coordinates of the initial spiral
     float xInit = end;
@@ -188,42 +198,45 @@ int main(int argc, char *argv[]) {
     float xSpaced = cos(2*M_PI*xInit/spacing)*xInit;
     float ySpaced = sin(2*M_PI*xInit/spacing)*xInit;
 
-    // Create an angle variable to hold the angle
-    float angle = 0.00;
+    // Create an angle variable to calculate the angle difference between a normal spiral and a spacing adjusted one
+    float angle = acosf( ( (xInit * xSpaced) + (yInit * ySpaced) ) / ( sqrt( powf(xInit,2) + powf(yInit,2) ) * sqrt( powf(xSpaced,2) + powf(ySpaced,2) ) ) );
 
-    // Check the y Pos of the ySpaced
-    if (ySpaced<0) {
-        // Calculate the Angle between two vectors (Coordinates)
-        angle = -acosf( ( (xInit * xSpaced) + (yInit * ySpaced) ) / ( sqrt( powf(xInit,2) + powf(yInit,2) ) * sqrt( powf(xSpaced,2) + powf(ySpaced,2) ) ) );
-    } else {
-        angle = acosf( ( (xInit * xSpaced) + (yInit * ySpaced) ) / ( sqrt( powf(xInit,2) + powf(yInit,2) ) * sqrt( powf(xSpaced,2) + powf(ySpaced,2) ) ) );
-    }
+    // Adjust the angle orientation according to spacing adjusted y-coordinate
+    ySpaced < 0 ? angle *= -1 : angle;
 
     // Adjust the angle according to user's rotation preference
     angle += rotate;    // In radians
     /* --- End of ANGLE --- */
 
     /* --- GENERATE COIL --- */
+    /* Generate the coils with the given parameters per each layer.
+     * Every two layers is rotated with an angle (viaAngle) associated with the 
+     * number of layers. This ensures for better placement of vias and better wiring.
+     */
+
+    // Print out the User Parameters used to create the coils.
     printf("\n --- Parameters Entered: --- \n");
     printf("turns:\t\t%.3f\nInner Radius:\t%.3f\nSpacing:\t%.3f\nStart_X:\t%.3f\nStart_Y:\t%.3f\nLayers:\t\t%d\nDirection:\t%d\nRotation:\t%.3f\nWidth:\t\t%.3f\nnetID:\t\t%d\nviaSize:\t%.3f\n\r",turns,innerRadius,spacing-width,startX,startY,layers,direction,rotate,width,netID,viaSize);
     printf(" --------------------------- \n");
 
+    // Print out the loading screen
     printf("\n --- Generating Coils --- \n");
 
     // Step size of the coil generator
     float step = ( (float)0.01 / (start) );
 
     // Calculate the angle between the via positions for different layer combinations
-    float viaAngle = ( 2*M_PI ) / ( ceilf( (float)(layers-1)/2 ) );
+    float viaAngle = ( 2*M_PI ) / ( ceilf( (float)(layers-0.5)/2 ) );
 
     // Initialize the Position Arrays
     float xPos[layers][(int)((end+(layers-1)*viaAngle*(spacing)/(2*M_PI)-start)/step + 1)];
     float yPos[layers][(int)((end+(layers-1)*viaAngle*(spacing)/(2*M_PI)-start)/step + 1)];
 
+    // Dummy variables for the loop
     float x, coilInitX, coilInitY, coilFixedX, coilFixedY, coilAngledX, coilAngledY;
 
-    int layerCode = 0;
-    int angleCode = 0;
+    // Layer Adjuster
+    int layerCode;
 
     // Iterate through each copper layer
     for (int i = 0; i < layers; i++) {
@@ -231,33 +244,33 @@ int main(int argc, char *argv[]) {
         // Print out progress tag
         printf("Progress:\e[s");
 
-        if (i > 1) {
-            layerCode = powf( 2, floor( (float)i/2 - 0.5 ) );
-        }
+        // Adjust the layer numbering for specfic end locations required per layer
+        layerCode = floor( i/2 );
         
         // Loop through each step to find the position
-        for (int j = 0; j < (int)(((end+(layerCode)*viaAngle*(spacing)/(2*M_PI))-start)/step + 1); j++) {
+        for (int j = 0; j < (int)(((end+pow(-1, i)*(layerCode)*viaAngle*(spacing)/(2*M_PI))-start)/step + 1); j++) {
 
-            x = j*step + start;   // Respective position
+            x = j*step + start;   // Respective position (can think of it as x variable in a calculator while plotting this.)
 
+            // X & Y coordinate variables for the initial coil with spacing
             coilInitX = cos(2*M_PI*x/spacing)*x;
             coilInitY = sin(2*M_PI*x/spacing)*x;
 
+            // Adjusted X & Y coordinate variables for the angle adjusted coil with spacing
             coilFixedX = direction * (cos(angle)*coilInitX + sin(angle)*coilInitY);
             coilFixedY = pow(-1, i) * (-sin(angle)*coilInitX + cos(angle)*coilInitY);
 
-            angleCode = floor( i/2 );
+            // Correctly orient the coil on each layer to have a nice via layout
+            coilAngledX = cos((layerCode)*viaAngle)*coilFixedX + sin((layerCode)*viaAngle)*coilFixedY;
+            coilAngledY = -sin((layerCode)*viaAngle)*coilFixedX + cos((layerCode)*viaAngle)*coilFixedY;
 
-            coilAngledX = cos((angleCode)*viaAngle)*coilFixedX + sin((angleCode)*viaAngle)*coilFixedY;
-            coilAngledY = -sin((angleCode)*viaAngle)*coilFixedX + cos((angleCode)*viaAngle)*coilFixedY;
-
-            // Use the spiral formula 
-            xPos[i][j] = direction * coilAngledX + startX;
+            // Store the final X & Y coordinates plus initial position coordinate.
+            xPos[i][j] = coilAngledX + startX;
             yPos[i][j] = coilAngledY + startY;
-            // Print out the progress
-            printf(" %d %2d (%.2f%%)\e[u", i+1, j, roundf(((float) j / (((end+(layerCode)*viaAngle*(spacing)/(2*M_PI))-start)/step + 1)) * 100));
-        }
 
+            // Print out the progress
+            printf(" %d %2d (%.2f%%)\e[u", i+1, j, roundf(((float) j / (((end+pow(-1, i)*(layerCode)*viaAngle*(spacing)/(2*M_PI))-start)/step + 1)) * 100));
+        }
         printf("\n");
     }
 
@@ -266,21 +279,25 @@ int main(int argc, char *argv[]) {
     /* --- End of GENERATE COIL --- */
 
     /* --- WRITE --- */
-    printf("\n --- Writing into File --- \n");
+    /* Using the xPos & yPos 2D arrays write all the coordinates generated
+     * into the file opened using KiCAD footprint format.
+     * Adjust the locations of the vias and write the coordinates of the vias
+     * into the file using the KiCAD footprint format.
+     */
 
-    layerCode = 0;
+    // Print out the loading screen
+    printf("\n --- Writing into File --- \n");
 
     // Iterate through each copper layer
     for (int i = 0; i < layers; i++) {
         // Print out progress tag
         printf("Progress:\e[s");
 
-        if (i > 1) {
-            layerCode = powf( 2, floor( (float)i/2 - 0.5 ) );
-        }
+        // Adjust the layer numbering for specfic end locations required per layer
+        layerCode = floor( i/2 );
 
         // Iterate through each position on the coil
-        for (int j = 0; j < (int)(((end+(layerCode)*viaAngle*(spacing)/(2*M_PI))-start)/step); j++) {
+        for (int j = 0; j < (int)(((end+pow(-1, i)*(layerCode)*viaAngle*(spacing)/(2*M_PI))-start)/step); j++) {
             // Print out the wire segments according to KiCAD Footprint File.
 
             // Check for copper layers
@@ -292,42 +309,42 @@ int main(int argc, char *argv[]) {
                 fprintf(fp, "(segment (start %f %f) (end %f %f) (width %f) (layer \"In%d.Cu\") (net %d) (tstamp 4efbfedb-0d6a-488e-863f-1beaaa%dba%d))\n", xPos[i][j], yPos[i][j], xPos[i][j+1], yPos[i][j+1], width, i, netID, j, i);
             }
             // Print out the progress
-            printf(" %d %2d (%.2f%%)\e[u", i+1, j, roundf(((float) j / (((end+(layerCode)*viaAngle*(spacing)/(2*M_PI))-start)/step)) * 100));
+            printf(" %d %2d (%.2f%%)\e[u", i+1, j, roundf(((float) j / (((end+pow(-1, i)*(layerCode)*viaAngle*(spacing)/(2*M_PI))-start)/step)) * 100));
         }
         printf("\n");
-
     }
 
+    // Create a unit vector pointing to the via locations
     float unitVector[2] = {xPos[0][0]/(sqrt(powf(xPos[0][0],2) + powf(yPos[0][0],2))), yPos[0][0]/(sqrt(powf(xPos[0][0],2) + powf(yPos[0][0],2)))};
 
-    if (layers == 1) {
-        
-        // Add vias
+    // Create vias at specific locations while biasing the location towards the center of the coil
+    if (layers == 1) { 
+        // Add a via adjusted using the unit vector
         fprintf(fp,"(via (at %f %f) (size 0.8) (drill 0.4) (layers \"F.Cu\" \"B.Cu\") (free) (net %d) (tstamp e5f06cd2-492e-41b2-8ded-13a3fa1042b%d))\n", xPos[0][0] + ( unitVector[0] * (-viaSize/2 + width/2) ), yPos[0][0] + ( unitVector[1] * (-viaSize/2 + width/2) ), netID, 0);
 
     } else {
-
-        
-
         for (int i = 0; i < layers-1; i++) {
             // Add vias
             if ((i+1) % 2) {
+                // Adjust the unit vector for the new via position
                 unitVector[0] = xPos[i][0]/(sqrt(powf(xPos[i][0],2) + powf(yPos[i][0],2)));
                 unitVector[1] = yPos[i][0]/(sqrt(powf(xPos[i][0],2) + powf(yPos[i][0],2)));
+
+                // Adjust the via position using the new unit vector
                 fprintf(fp,"(via (at %f %f) (size 0.8) (drill 0.4) (layers \"F.Cu\" \"B.Cu\") (free) (net %d) (tstamp e5f06cd2-492e-41b2-8ded-13a3fa1042b%d))\n", xPos[i][0] + ( unitVector[0] * (-viaSize*2/3 + width/2) ), yPos[i][0] + ( unitVector[1] * (-viaSize*2/3 + width/2) ), netID, 0);
             }
-            
         }
-
     }
 
-    
-    
     printf(" ------------------------- \n");
     printf("End of writing.\n\n\r");
     /* --- End of WRITE --- */
 
     /* --- DISPLAY --- */
+    /* Display any additional information.
+     */
+
+    // Give a warning message to warn the user for adjusting the number of copper layers on KiCAD
     if (layers > 2) {
         printf("\n\rYou have selected more than 2 copper layers.\n\rPlease make sure to change the number of copper layers on KiCAD and make sure the copper layer names matches.\n\n\r");
     }
@@ -340,4 +357,3 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 /* --- End of MAIN --- */
-
